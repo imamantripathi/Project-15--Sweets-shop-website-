e1f7dfefb06b6c44d131284e0640d526ea1582d6
Aman Tripathi <imamantripathi@gmail.com>
2022-08-04 17:00:50 +0530

    Navbar Complete

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,20 @@
-let NavHoverEffects = document.querySelectorAll("nav ul li a");
-NavHoverEffects.forEach(function (NavHoverEffect) {
-  NavHoverEffect.addEventListener("mouseover", () => {
-    event.target.style.color = "yellow";
-  });
-  NavHoverEffect.addEventListener("mouseout", () => {
-    event.target.style.color = "white";
-  });
-  
+// let NavHoverEffects = document.querySelectorAll("nav ul li a");
+// NavHoverEffects.forEach(function (NavHoverEffect) {
+//     NavHoverEffect.addEventListener("mouseover", () => {
+//         event.target.style.color = "#d5fefd";
+//     });
+//     NavHoverEffect.addEventListener("mouseout", () => {
+//         event.target.style.color = "#fffcff";
+//     });
+// });
+let navBtn = document.querySelector(".navbar-toggle");
+let navbar = document.querySelector("#navbar");
+navBtn.addEventListener("click", () => {
+  //   navbar.style.display = "block";
+
+  if (navbar.style.display === "none") {
+    navbar.style.display = "block";
+  } else {
+    navbar.style.display = "none";
+  }
 });
