19205d60a379590b4eed09fc47e78980d0e6fd80
Aman Tripathi <imamantripathi@gmail.com>
2022-10-09 16:10:03 +0530

    Review Section

--- JS/script.js
+++ JS/script.js
@@ -27,15 +27,48 @@ function showSlides() {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
+// script of review section
+var btn = document.getElementsByClassName("btn");
+var slide = document.getElementById("slide");
+btn[0].onclick = function () {
+  slide.style.transform = "translateX(0px)";
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[1].onclick = function () {
+  slide.style.transform = "translateX(-800px)";
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[2].onclick = function () {
+  slide.style.transform = "translateX(-1600px)";
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
+btn[3].onclick = function () {
+  slide.style.transform = "translateX(-2400px)";
+  for (i = 0; i < 4; i++) {
+    btn[i].classList.remove("active");
+  }
+  this.classList.add("active");
+};
